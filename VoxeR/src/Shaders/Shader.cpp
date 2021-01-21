6f0d298ca43d3097e714e6adf2ad15ce7734ea7d
Igor Zhigulin <74398851+igor-zhigulin@users.noreply.github.com>
2021-01-21 07:37:05 +0700

    Uniform-переменные (#12)
    Uniform-переменные
    
    Доработал Uniform-переменные, но не тестировал. Но, вроде как, должно все работать. Также добавил пару get вызовов, чтобы получить позицию uniform'ов и атрибутов.

--- VoxeR/src/Shaders/Shader.cpp
+++ VoxeR/src/Shaders/Shader.cpp
@@ -87,6 +87,7 @@ GLuint Shader::Shader::getShader() const
 }
 
 Shader::Program::Program()
+	: ready(false)
 {
 	program = glCreateProgram();
 }
@@ -111,12 +112,272 @@ bool Shader::Program::linkShaders()
 	glGetProgramiv(program, GL_LINK_STATUS, &success);
 
 	if (success)
-		return true;
-	else
-		return false;
+		ready = true;
+	return success;
 }
 
-void Shader::Program::use() const
+bool Shader::Program::use() const
 {
+	if (!ready)
+		return false;
 	glUseProgram(program);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLfloat v0)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform1f(location, v0);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLfloat v0, GLfloat v1)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform2f(location, v0, v1);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLfloat v0, GLfloat v1, GLfloat v2)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform3f(location, v0, v1, v2);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform4f(location, v0, v1, v2, v3);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLint v0)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform1i(location, v0);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLint v0, GLint v1)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform2i(location, v0, v1);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLint v0, GLint v1, GLint v2)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform3i(location, v0, v1, v2);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLint v0, GLint v1, GLint v2, GLint v3)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform4i(location, v0, v1, v2, v3);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLuint v0)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform1ui(location, v0);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLuint v0, GLuint v1)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform2ui(location, v0, v1);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLuint v0, GLuint v1, GLuint v2)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform3ui(location, v0, v1, v2);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLuint v0, GLuint v1, GLuint v2, GLuint v3)
+{
+	if (!use() || location < 0)
+		return false;
+	glUniform4ui(location, v0, v1, v2, v3);
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLsizei count, int vector_size, const GLfloat* value)
+{
+	if (!use() || !value || count <= 0 || location < 0)
+		return false;
+	switch (vector_size)
+	{
+	case 1:
+		glUniform1fv(location, count, value);
+		break;
+	case 2:
+		glUniform2fv(location, count, value);
+		break;
+	case 3:
+		glUniform3fv(location, count, value);
+		break;
+	case 4:
+		glUniform4fv(location, count, value);
+		break;
+	default:
+		return false;
+	}
+	if (glGetError() != GL_NO_ERROR)
+		return false;
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLsizei count, int vector_size, const GLint* value)
+{
+	if (!use() || !value || count <= 0 || location < 0)
+		return false;
+	switch (vector_size)
+	{
+	case 1:
+		glUniform1iv(location, count, value);
+		break;
+	case 2:
+		glUniform2iv(location, count, value);
+		break;
+	case 3:
+		glUniform3iv(location, count, value);
+		break;
+	case 4:
+		glUniform4iv(location, count, value);
+		break;
+	default:
+		return false;
+	}
+	if (glGetError() != GL_NO_ERROR)
+		return false;
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLsizei count, int vector_size, const GLuint* value)
+{
+	if (!use() || !value || count <= 0 || location < 0)
+		return false;
+	switch (vector_size)
+	{
+	case 1:
+		glUniform1uiv(location, count, value);
+		break;
+	case 2:
+		glUniform2uiv(location, count, value);
+		break;
+	case 3:
+		glUniform3uiv(location, count, value);
+		break;
+	case 4:
+		glUniform4uiv(location, count, value);
+		break;
+	default:
+		return false;
+	}
+	if (glGetError() != GL_NO_ERROR)
+		return false;
+	return true;
+}
+
+bool Shader::Program::uniform(GLint location, GLsizei count, GLboolean transpose, int rows, int cols, const GLfloat* value)
+{
+	if (!use() || location < 0 || count <= 0 || !value)
+		return false;
+
+	if (rows == 2)
+	{
+		switch (cols)
+		{
+		case 2:
+			glUniformMatrix2fv(location, count, transpose, value);
+			break;
+		case 3:
+			glUniformMatrix2x3fv(location, count, transpose, value);
+			break;
+		case 4:
+			glUniformMatrix2x4fv(location, count, transpose, value);
+			break;
+		default:
+			return false;
+			break;
+		}
+	}
+	else if (rows == 3)
+	{
+		switch (cols)
+		{
+		case 2:
+			glUniformMatrix3x2fv(location, count, transpose, value);
+			break;
+		case 3:
+			glUniformMatrix3fv(location, count, transpose, value);
+			break;
+		case 4:
+			glUniformMatrix3x4fv(location, count, transpose, value);
+			break;
+		default:
+			return false;
+			break;
+		}
+	}
+	else if (rows == 4)
+	{
+		switch (cols)
+		{
+		case 2:
+			glUniformMatrix4x2fv(location, count, transpose, value);
+			break;
+		case 3:
+			glUniformMatrix4x3fv(location, count, transpose, value);
+			break;
+		case 4:
+			glUniformMatrix4fv(location, count, transpose, value);
+			break;
+		default:
+			return false;
+			break;
+		}
+	}
+	else
+	{
+		return false;
+	}
+	if (glGetError() != GL_NO_ERROR)
+		return false;
+	return true;
+}
+
+GLint Shader::Program::getUniformLocation(const char* uniform_name) const
+{
+	if (!uniform_name)
+		return -1;
+	
+	return glGetUniformLocation(program, uniform_name);
+}
+
+GLint Shader::Program::getAttribLocation(const char* attribute_name) const
+{
+	if (!attribute_name)
+		return -1;
+
+	return glGetAttribLocation(program, attribute_name);
 }
--- VoxeR/src/Shaders/Shader.h
+++ VoxeR/src/Shaders/Shader.h
@@ -37,11 +37,36 @@ namespace Shader
 	{
 	private:
 		GLuint program{ 0 };
+		bool ready{ false };
 	public:
 		Program();
 		bool attachShader(Shader);
 		bool linkShaders();
-		void use() const;
+		bool use() const;
+
+		bool uniform(GLint location, GLfloat v0);
+		bool uniform(GLint location, GLfloat v0, GLfloat v1);
+		bool uniform(GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
+		bool uniform(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+
+		bool uniform(GLint location, GLint v0);
+		bool uniform(GLint location, GLint v0, GLint v1);
+		bool uniform(GLint location, GLint v0, GLint v1, GLint v2);
+		bool uniform(GLint location, GLint v0, GLint v1, GLint v2, GLint v3);
+
+		bool uniform(GLint location, GLuint v0);
+		bool uniform(GLint location, GLuint v0, GLuint v1);
+		bool uniform(GLint location, GLuint v0, GLuint v1, GLuint v2);
+		bool uniform(GLint location, GLuint v0, GLuint v1, GLuint v2, GLuint v3);
+
+		bool uniform(GLint location, GLsizei count, int size, const GLfloat* value);
+		bool uniform(GLint location, GLsizei count, int size, const GLint* value);
+		bool uniform(GLint location, GLsizei count, int size, const GLuint* value);
+
+		bool uniform(GLint location, GLsizei count, GLboolean transpose, int rows, int cols, const GLfloat* value);
+
+		GLint getUniformLocation(const char* uniform_name) const;
+		GLint getAttribLocation(const char* attribute_name) const;
 	};
 
 }
